2de34db4db7c4ab8645c1c075027022d451c1acb
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2024-06-13 12:32:31 +0200

    tokenfetching

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    serverRuntimeConfig: {
+        // Will only be available on the server side
+        flexjarBackendClientId: process.env.FLEXJAR_BACKEND_CLIENT_ID,
+    },
+}
 
 module.exports = nextConfig
--- src/fetching/flexjarFetching.ts
+++ src/fetching/flexjarFetching.ts
@@ -1,14 +1,46 @@
+import { logger } from '@navikt/next-logger'
+import getConfig from 'next/config'
+
+import { requestAzureClientCredentialsToken } from '@/auth/client-credentials'
+import { isLocalOrDemo } from '@/utlis/env'
+
+const { serverRuntimeConfig } = getConfig()
+
 export async function hentFlexjarFeedbacks(): Promise<Feedback[]> {
+    if (isLocalOrDemo) {
+        return [
+            {
+                feedback: 'feedback',
+                svar: 'svar',
+                app: 'app',
+                feedbackId: 'feedbackId',
+                opprettet: '2024-01-01',
+            },
+            {
+                feedback: 'hei',
+                svar: '5',
+                app: 'app',
+                feedbackId: 'feedbackId',
+                opprettet: '2024-01-01',
+            },
+        ]
+    }
+    const token = await requestAzureClientCredentialsToken(serverRuntimeConfig.flexjarBackendClientId)
+    if (!token.ok) {
+        logger.error('Kunne ikke hente token: ' + token.error)
+        return [
+            {
+                feedback: 'Klarte ikke hente feedbacks fra backend. error',
+                svar: '1',
+                app: 'app',
+                feedbackId: 'feedbackId',
+                opprettet: '2024-01-01',
+            },
+        ]
+    }
     return [
         {
-            feedback: 'feedback',
-            svar: 'svar',
-            app: 'app',
-            feedbackId: 'feedbackId',
-            opprettet: '2024-01-01',
-        },
-        {
-            feedback: 'hei',
+            feedback: 'Klarte å hente token til bruk',
             svar: '5',
             app: 'app',
             feedbackId: 'feedbackId',
