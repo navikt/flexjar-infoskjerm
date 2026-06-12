678b6293af57be2b4a30caf5416b8a994d5ac830
Ole Bastian Løchen <42940656+olebaba@users.noreply.github.com>
2026-06-12 13:08:10 +0200

    Rydd opp i fetch og config (#1122)

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    serverRuntimeConfig: {
-        // Will only be available on the server side
-        flexjarBackendClientId: process.env.FLEXJAR_BACKEND_CLIENT_ID,
-    },
-}
-
-module.exports = nextConfig
